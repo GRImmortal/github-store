5c2cc7365209b2771394a3790a3c328cc2ae9ef2
unknown <13920329093@163.com>
2021-09-16 13:09:06 +0800

    hello

--- hello.py
+++ hello.py
@@ -1,1 +1,7 @@
-中文测试
+# -*- coding: utf-8 -*-
+print('Hello, %s, 成绩提升了%s %%' % ('小明',17.1))
+r = 2.5
+s = 3.14*r**2
+print(f'The area of a circle with radius {r} is {s:.2f}') #s:.2f表示取s所代表数字的小数点后两位
+
+print('Hello, %s, 成绩提升了%s %%' % ('小明',(85-72)*100//72))
