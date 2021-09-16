a742a8ebedeacb2ca36e8d3b46b616a895cf0f46
unknown <13920329093@163.com>
2021-09-16 14:26:00 +0800

    add 小明

--- hello.py
+++ hello.py
@@ -5,3 +5,7 @@ s = 3.14*r**2
 print(f'The area of a circle with radius {r} is {s:.2f}') #s:.2f表示取s所代表数字的小数点后两位
 
 print('Hello, %s, 成绩提升了%s %%' % ('小明',(85-72)*100//72))
+s1=72
+s2=85
+r = (s2-s1)*100/s1
+print(f'小明的成绩从去年的{s1}分提升到了今年的{s2}分,成绩提升了{r:.1f}%')
